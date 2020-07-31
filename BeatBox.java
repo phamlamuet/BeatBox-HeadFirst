1ed6296f19b47afe8909feda68d20480ad1260d9
Lam Pham <phamlamuet@gmail.com>
2020-08-01 02:17:03 +0700

    add a commit

---
+++ BeatBox.java
@@ -0,0 +1,185 @@
+package phamlamuet;
+
+import javax.sound.midi.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class BeatBox {
+    JPanel mainPanel;
+    ArrayList<JCheckBox> checkboxList;//we store checkboxes in an ArrayList
+    Sequencer sequencer;
+    Sequence sequence;
+    Track track;
+    JFrame theFrame;
+
+    String[] instrumentNames = {"Bass Drum", "Closed Hi-Hat", "Open Hi-Hat", "Acoustic Snare",
+            "Crash Cynbal", "Hand Clap", "High Tom", "Hi Bongo", "Maracas", "Whitsle", "Low Conga", "CowBell",
+            "Vibraslap", "Low-mid Tom", "High Agogo", "Open Hi Conga"
+    };
+    int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
+
+    public static void main(String[] args) {
+        new BeatBox().buildGUI();
+    }
+
+    public void buildGUI() {
+        theFrame = new JFrame("Cyber BeatBox");
+        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        BorderLayout layout = new BorderLayout();
+        JPanel background = new JPanel(layout);
+        background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        //an empty border give us a margin
+        checkboxList = new ArrayList<JCheckBox>();
+        Box buttonBox = new Box(BoxLayout.Y_AXIS);
+
+        JButton start = new JButton("Start");
+        start.addActionListener(new MyStartListener());
+        buttonBox.add(start);
+
+        JButton stop = new JButton("Stop");
+        stop.addActionListener(new MyStopListener());
+        buttonBox.add(stop);
+
+        JButton upTempo = new JButton("Tempo Up");
+        upTempo.addActionListener(new MyUpTempoListener());
+        buttonBox.add(upTempo);
+
+        JButton downTempo = new JButton("Tempo Down");
+        downTempo.addActionListener(new MyDownTempoListener());
+        buttonBox.add(downTempo);
+
+        Box nameBox = new Box(BoxLayout.Y_AXIS);
+        for (int i = 0; i < 16; i++) {
+            nameBox.add(new Label(instrumentNames[i]));
+        }
+
+        background.add(BorderLayout.EAST, buttonBox);
+        background.add(BorderLayout.WEST, nameBox);
+
+        theFrame.getContentPane().add(background);
+
+        GridLayout grid = new GridLayout(16, 16);
+        grid.setVgap(1);
+        grid.setHgap(2);
+        mainPanel = new JPanel(grid);
+        background.add(BorderLayout.CENTER, mainPanel);
+
+        for (int i = 0; i < 256; i++) {            //make the checkboxes,set them to 'false' as default
+            JCheckBox c = new JCheckBox();//and add them to the ArrayList and to the GUI Panel
+            c.setSelected(false);
+            checkboxList.add(c);
+            mainPanel.add(c);
+        }
+        setUpMidi();
+        theFrame.setBounds(50, 50, 300, 300);
+        theFrame.pack();
+        theFrame.setVisible(true);
+    }
+
+    public void setUpMidi() {
+        try {
+            sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            sequence = new Sequence(Sequence.PPQ, 4);
+            track = sequence.createTrack();
+            sequencer.setTempoInBPM(120);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void buildTrackAndStart() {
+        int[] trackList = null; //We'll make a 16-element arry to hold the values for one instrument,
+        //across all 16 beats
+        sequence.deleteTrack(track);//get rid of old track
+        track = sequence.createTrack();//make a fresh one
+
+        for (int i = 0; i < 16; i++) { //Do this for each 16 ROWS
+
+            trackList = new int[16];
+            int key = instruments[i];
+
+            for (int j = 0; j < 16; j++) {  //Do this for each of beat in this row
+
+                JCheckBox jc = (JCheckBox) checkboxList.get(j + (16 * i));
+
+                if (jc.isSelected()) {//if checkbox at this beat selected?If yes,put the key value
+                    trackList[j] = key;//in this slot in the array(the slot that represents this beat
+                } else {//Otherwise,the instrument is NOT supposed to play at this beat,so set it to zero
+                    trackList[j] = 0;
+                }
+            }
+
+            makeTracks(trackList);//for this instrument and for all 16 beats,make event and add them to the track
+            track.add(makeEvent(176, 1, 127, 0, 16));
+        }
+        track.add(makeEvent(192, 9, 1, 0, 15));
+        try {
+            sequencer.setSequence(sequence);
+            sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY);
+            sequencer.start();
+            sequencer.setTempoInBPM(120);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class MyStartListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            buildTrackAndStart();
+        }
+    }
+
+    public class MyStopListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            sequencer.stop();
+        }
+    }
+
+    public class MyUpTempoListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            float tempoFactor = sequencer.getTempoFactor();
+            sequencer.setTempoFactor((float) tempoFactor * 1.03f);
+        }
+    }
+
+    public class MyDownTempoListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            float tempoFactor = sequencer.getTempoFactor();
+            sequencer.setTempoFactor((float) (tempoFactor * 0.97f));
+        }
+    }
+
+    public void makeTracks(int[] list) {
+        for (int i = 0; i < 16; i++) {
+            int key = list[i];
+
+            if (key != 0) {
+                track.add(makeEvent(144,9,key,100,i));
+                track.add(makeEvent(128,9,key,100,i+1));
+            }
+        }
+    }
+
+    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+        MidiEvent event = null;
+        try {
+            ShortMessage a=new ShortMessage();
+            a.setMessage(comd,chan,one,two);
+            event=new MidiEvent(a,tick);
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return event;
+    }
+
+}
